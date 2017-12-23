0a9eb8f34bd9482371b4232763583de039c7536a
Mr_Ja <374520089@qq.com>
2017-12-23 22:41:11 +0800

    推荐页面

--- src/components/Recommend/Recommend.vue
+++ src/components/Recommend/Recommend.vue
@@ -7,10 +7,23 @@
 
 <script>
 import { Divider } from 'vux'
-
+import { getRecommend } from 'api/recommend'
+import { ERR_OK } from 'api/config'
 export default {
   components: {
     Divider
+  },
+  createrd () {
+    this.getRecommend()
+  },
+  methods: {
+  _getRecommend () {
+    getRecommend().then((res) => {
+      if (res.code === ERR_OK) {
+        console.log(res.data.slider)
+      }
+    })
+  }
   }
 }
 </script>
